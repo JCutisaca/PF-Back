e85a45e8b335c9250b2617e27aec7807e85726d5
msolangeg <marina.solange.g@gmail.com>
2023-10-05 00:12:11 -0300

    fix(updateUser): add userBan in destructured props

--- src/controllers/UserController/updateUser.js
+++ src/controllers/UserController/updateUser.js
@@ -2,10 +2,10 @@ const { User } = require('../../db');
 const getUserByID = require('./getUserById');
 const bcrypt = require('bcryptjs')
 
-const updateUser = async ({ id, name, surname, email, phone, password, address, typeUser }) => {
+const updateUser = async ({ id, name, surname, email, phone, password, address, typeUser, userBan }) => {
 
     if (!id) throw Error("Please provide a valid ID.")
-    if (!(name || surname || email || phone || password || address || typeUser, userBan)) throw Error("Please specify the information you want to update.")
+    if (!(name || surname || email || phone || password || address || typeUser || userBan)) throw Error("Please specify the information you want to update.")
     const findUser = await User.findOne({ where: { id } })
     if (!findUser) throw Error("User not found.")
     const update = await User.update({
