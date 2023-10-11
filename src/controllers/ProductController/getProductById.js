10dc60a650dd9f9952510cd19e83c96e3a5af339
MayDenhoff <mayradenhoff@gmail.com>
2023-10-11 20:33:24 -0300

    reset

--- src/controllers/ProductController/getProductById.js
+++ src/controllers/ProductController/getProductById.js
@@ -3,16 +3,20 @@ const { Product, Category, Review } = require('../../db')
 const getProductById = async ({ id }) => {
     if(!id) throw Error("Please provide a valid ID.")
     const productByID = await Product.findOne({
-        where: { id }, include: [Category], attributes: {
+        where: { id }, 
+        
+        
+        include: [Category], attributes: {
             exclude: ['CategoryId']
         }, include: [
             {
             model: Review,
             as: 'Reviews',
-          attributes: ['id','reviewText'], 
+          attributes: ['id','reviewText', 'rating'], 
         }
       ]
-    })
+    }
+    )
 
     if (!productByID) {
         throw new Error("Product not found.");
