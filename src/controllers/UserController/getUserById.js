99fe56d7527085b7e2469d99afe21252446691d9
MayDenhoff <mayradenhoff@gmail.com>
2023-10-12 02:33:08 -0300

    se agrego favoritos en userById

--- src/controllers/UserController/getUserById.js
+++ src/controllers/UserController/getUserById.js
@@ -1,8 +1,13 @@
-const { User } = require('../../db')
+const { User, Product } = require('../../db')
 
 const getUserByID = async({id}) => {
     if(!id) throw Error("Please provide a valid ID.")
-    const userById = await User.findOne({where: {id}})
+    const userById = await User.findByPk(id, {include: [
+        {
+          model: Product,
+          as: 'FavoriteProducts',
+        },
+      ]})
     return userById
 }
 
