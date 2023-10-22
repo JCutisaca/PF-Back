48ed64824ac7258a6012400035d3c7b5b28f7f57
soldier_lucas <3jhoncuco@gmail.com>
2023-10-22 13:37:17 -0300

    fixed deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { PORT } = process.env;
 const port = PORT || 3001;
 const host = "0.0.0.0";
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(port, host, () => {
     console.log(`Server listening on port ${port}`);
   })
